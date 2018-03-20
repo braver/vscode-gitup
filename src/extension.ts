bbbd34c0c5ee006ba4db2fa9f8bc6227f71f5c85
Joe Chrysler <mail@joechrysler.com>
2018-03-20 15:22:51 -0400

    Launch GitUp via the command line tool
    Opening GitUp with the `open` command works well for projects where the
    repository root and the VSCode workspace root path are the same. It
    doesn’t work for projects where the workspace root is a child of the
    repository root. The command line tool, included with GitUp, is built to
    handle this sort of situation gracefully.

--- src/extension.ts
+++ src/extension.ts
@@ -5,10 +5,10 @@ import child_process = require("child_process");
 
 export function activate(context: vscode.ExtensionContext): void {
   context.subscriptions.push(vscode.commands.registerCommand("extension.opengitup", () => {
-    var app = "/Applications/GitUp.app";
+    var commandLineTool = "/Applications/GitUp.app/Contents/SharedSupport/gitup";
     var project = vscode.workspace.rootPath;
     if (project) {
-      child_process.execFile("open", ["-a", app, project]);
+      child_process.exec(commandLineTool);
     }
   }));
 }
